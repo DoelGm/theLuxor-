573acd7abc19cf087b6fe26ef3c71edc130e98be
pipoyt <pipokueryt@gmail.com>
2025-05-01 15:25:31 -0600

    Sobre nosotros

--- src/app/pages/index/index.component.ts
+++ src/app/pages/index/index.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { WelcomeComponent } from "../../components/welcome/welcome.component";
+import { AboutUsComponent } from "../../components/about-us/about-us.component";
 
 @Component({
   selector: 'app-index',
-  imports: [WelcomeComponent],
+  imports: [WelcomeComponent, AboutUsComponent],
   templateUrl: './index.component.html',
   styleUrl: './index.component.css'
 })
