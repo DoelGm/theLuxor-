c3d150c687eb3821d967dd271254c4ce80509828
Rafael Bernal Gallegos <rbernalgallegos@gmail.com>
2025-05-01 16:15:02 -0600

    Estructura del welcome terminada

--- src/app/components/works/works.component.ts
+++ src/app/components/works/works.component.ts
@@ -1,11 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-works',
-  imports: [],
+  imports: [ CommonModule],
   templateUrl: './works.component.html',
   styleUrl: './works.component.css'
 })
 export class WorksComponent {
-
+  // Add any properties or methods you need for this component here
+  works = [
+    {
+      title: 'Project 1',
+      description: 'Description of Project 1',
+      imageUrl: 'assets/imgs/proyecto1.jpg'
+    },
+    {
+      title: 'Project 2',
+      description: 'Description of Project 2',
+      imageUrl: 'assets/imgs/proyecto2.jpg'
+    },
+    {
+      title: 'Project 3',
+      description: 'Description of Project 3',
+      imageUrl: 'assets/imgs/proyecto3.jpg'
+    }
+  ];
 }
