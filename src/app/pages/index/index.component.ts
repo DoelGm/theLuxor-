dac99614118928b7c5c8324bdee636816a5e5754
Rafael Bernal Gallegos <rbernalgallegos@gmail.com>
2025-05-05 14:04:26 -0600

    Cambio de color  de las cards de los proyectos

--- src/app/pages/index/index.component.ts
+++ src/app/pages/index/index.component.ts
@@ -3,11 +3,11 @@ import { WelcomeComponent } from "../../components/welcome/welcome.component";
 import { AboutUsComponent } from "../../components/about-us/about-us.component";
 import { WorksComponent } from "../../components/works/works.component";
 import { StatisticsComponent } from "../../components/statistics/statistics.component";
-import { ContactComponent } from "../../components/contact/contact.component";
+
 
 @Component({
   selector: 'app-index',
-  imports: [WelcomeComponent, AboutUsComponent, WorksComponent, StatisticsComponent, ContactComponent],
+  imports: [WelcomeComponent, AboutUsComponent, WorksComponent, StatisticsComponent],
   templateUrl: './index.component.html',
   styleUrl: './index.component.css'
 })
